5d43425629edfddf763ab387b37e467765995552
Maria Ricci <mery.ricci.mr@gmail.com>
2021-01-20 14:36:48 +0100

    improved data naming and attributes

--- prototype/data.js
+++ prototype/data.js
@@ -2,7 +2,7 @@ var storylines = {
     "cook" : {
         'title': 'Cook\'s third voyage',
         'description': 'Follow Cook\'s journey in a story map.<br/><span style="font-size: .8em;">text & images &copy; Wikipedia</span>',
-        "lines_name": 'Cook3dVoyageLine',
+        "lines_name": 'CookLine',
         "points_name": 'cookPoints',
         "timeline_html": 'color: blue',
         "color": "blue"
@@ -15,5 +15,30 @@ var storylines = {
         "timeline_start": '<div class="chapter" name="start" data-lon="-4.160823358060026" data-lat="50.326446898130285" data-zoom="4" data-animation="flyto"><h2>The third Cook Voyage 1776 - 1779</h2><p>Follow Cook\'s journey in a story map.<br/><span style="font-size: .8em;">text & images &copy; Wikipedia</span></p><div class="ol-scroll-next"><span>Start</span></div></div>',
         "timeline_html": 'color: red',
         "color": "red"
+    },
+    "pondLife" : {
+        'title': 'Peter Pond, Athabasca\'s early explorer',
+        'description': 'Discover life and exploration of Peter Pond, a controversial figure of the 18th century, who inspired Mackenzie to search the North West Passage.',
+        'lines_name': '',
+        'points_name': 'pondLifePoints',
+        "timeline_html": 'color: green',
+        "img": '',
+        "color": "green"
+    },
+    "castner" : {
+        'title': 'Brian Castner paddling the Mackenzie river in 2016',
+        'description': '',
+        "lines_name": '',
+        "points_name": 'castnerPoints',
+        "timeline_html": 'color: black',
+        "color": "black"
+    },
+    "mackenzieLife" : {
+        'title': 'Alexander Mackenzie: from the Highlands to the North West Territories',
+        'description': '',
+        "lines_name": '',
+        "points_name": 'mackenzieLifePoints',
+        "timeline_html": 'color: black',
+        "color": "black"
     }
 }
