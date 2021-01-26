efaa9828354778b5fe51eab7c12519c38392aef8
s6nocrem <75442659+s6nocrem@users.noreply.github.com>
2021-01-26 11:19:21 +0100

    Add files via upload
    this file is for adding the information layer with popup

---
+++ js/info-layer.js
@@ -0,0 +1,61 @@
+// define layers outside the function
+// Popup overlay
+
+var popup = new ol.Overlay.Popup (
+  {	popupClass: "default", //"tooltips", "warning" "black" "default", "tips", "shadow",
+    closeBox: true,
+    onshow: function(){ console.log("You opened the box"); },
+    onclose: function(){ console.log("You close the box"); },
+    positioning: 'top-center',
+    autoPan: true,
+    autoPanAnimation: { duration: 250 }
+  });
+
+// Vector Layer
+  var infoSource = new ol.source.Vector({
+  url: 'http://localhost:8080/geoserver/explore/wfs?service=WFS&' +
+    'version=1.1.0' +
+      '&request=GetFeature' +
+      '&typename=explore:extra_points_layer' +
+    '&outputFormat=application/json',
+  format: new ol.format.GeoJSON()
+});
+
+ var infoLayer = new ol.layer.Vector({
+   name: 'Additional information points',
+   source: infoSource
+   //style: new ol.style.Style({ image: new ol.style.Icon({ src:"exclamation-mark.png", scale: 0.05 }) })
+ });
+
+// Control Select
+var select = new ol.interaction.Select({});
+
+// On selected => show/hide popup
+select.getFeatures().on(['add'], function(e) {
+  var feature = e.element;
+  var content = "";
+  content += "<img src='"+feature.get("img")+"'/>";
+  content += feature.get("text");
+  content += "<p>";
+  content += feature.get("url_text");
+  if (feature.get("url") != "") {
+  content += "<a href="+feature.get("url")+" title='Info'> here.</a>";
+  }
+  popup.show(feature.getGeometry().getFirstCoordinate(), content);
+});
+select.getFeatures().on(['remove'], function(e) {
+  popup.hide();
+});
+
+ function show_info_layer() {
+     map.addOverlay(popup);
+     window.topLayer = infoLayer;
+     map.addLayer(infoLayer);
+     map.addInteraction(select);
+ }
+
+ function hide_info_layer() {
+     map.removeLayer(infoLayer);
+     map.deleteOverlay(popup);
+     map.removeInteraction(select);
+ }
