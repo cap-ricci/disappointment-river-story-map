91cca01acc32388fae2b566d7307f6eb18dd9a8f
s6nocrem <75442659+s6nocrem@users.noreply.github.com>
2021-02-01 15:08:20 +0100

    Update data.js
    changed colours

--- js/data.js
+++ js/data.js
@@ -5,8 +5,8 @@ var storylines = {
         'description': 'Follow Cook\'s journey in a story map.<br/><span style="font-size: .8em;">text & images &copy; Wikipedia</span>',
         "lines_name": 'CookLine',
         "points_name": 'cookPoints',
-        "timeline_html": 'color: blue',
-        "color": "blue"
+        "timeline_html": 'color: lightcoral',
+        "color": "lightcoral"
     },
     "hearne" : {
         'title': 'Coppermine River exploration',
@@ -15,32 +15,32 @@ var storylines = {
         'lines_name': 'hearneLine',
         'points_name': 'hearnePoints',
         "timeline_start": '<div class="chapter" name="start" data-lon="-4.160823358060026" data-lat="50.326446898130285" data-zoom="4" data-animation="flyto"><h2>The third Cook Voyage 1776 - 1779</h2><p>Follow Cook\'s journey in a story map.<br/><span style="font-size: .8em;">text & images &copy; Wikipedia</span></p><div class="ol-scroll-next"><span>Start</span></div></div>',
-        "timeline_html": 'color: red',
-        "color": "red"
+        "timeline_html": 'color: turquoise',
+        "color": "turquoise"
     },
     // "pondLife" : {
     //     'title': 'Peter Pond, Athabasca\'s early explorer',
     //     'description': 'Discover life and exploration of Peter Pond, a controversial figure of the 18th century, who inspired Mackenzie to search the North West Passage.',
     //     'lines_name': '',
     //     'points_name': 'pondLifePoints',
-    //     "timeline_html": 'color: green',
+    //     "timeline_html": 'color: cornflowerblue',
     //     "img": '',
-    //     "color": "green"
+    //     "color": "cornflowerblue"
     // },
     // "castner" : {
     //     'title': 'Brian Castner paddling the Mackenzie river in 2016',
     //     'description': '',
     //     "lines_name": '',
     //     "points_name": 'castnerPoints',
-    //     "timeline_html": 'color: black',
-    //     "color": "black"
+    //     "timeline_html": 'color: orchid',
+    //     "color": "orchid"
     // },
     // "mackenzieLife" : {
     //     'title': 'Alexander Mackenzie: from the Highlands to the North West Territories',
     //     'description': '',
     //     "lines_name": '',
     //     "points_name": 'mackenzieLifePoints',
-    //     "timeline_html": 'color: black',
-    //     "color": "black"
+    //     "timeline_html": 'color: chocolate',
+    //     "color": "chocolate"
     // }
 }
