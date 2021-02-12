887256ad68aeb5006a01d50debdb9999e367a001
Maria Ricci <mery.ricci.mr@gmail.com>
2021-02-12 18:36:09 +0100

    updated data

--- js/data.js
+++ js/data.js
@@ -14,27 +14,28 @@ var storylines = {
         'description': 'Follow Samuel Hearne\'s exploarion of the Coppermine River with native Chipewyan chief Matonabbee.',
         'lines_name': 'hearneLine',
         'points_name': 'hearnePoints',
-        "timeline_start": '<div class="chapter" name="start" data-lon="-4.160823358060026" data-lat="50.326446898130285" data-zoom="4" data-animation="flyto"><h2>The third Cook Voyage 1776 - 1779</h2><p>Follow Cook\'s journey in a story map.<br/><span style="font-size: .8em;">text & images &copy; Wikipedia</span></p><div class="ol-scroll-next"><span>Start</span></div></div>',
         "timeline_html": 'color: turquoise',
         "color": "turquoise"
     },
-    // "pondLife" : {
-    //     'title': 'Peter Pond, Athabasca\'s early explorer',
-    //     'description': 'Discover life and exploration of Peter Pond, a controversial figure of the 18th century, who inspired Mackenzie to search the North West Passage.',
-    //     'lines_name': '',
-    //     'points_name': 'pondLifePoints',
-    //     "timeline_html": 'color: cornflowerblue',
-    //     "img": '',
-    //     "color": "cornflowerblue"
-    // },
-    // "castner" : {
-    //     'title': 'Brian Castner paddling the Mackenzie river in 2016',
-    //     'description': '',
-    //     "lines_name": '',
-    //     "points_name": 'castnerPoints',
-    //     "timeline_html": 'color: orchid',
-    //     "color": "orchid"
-    // },
+    "pondLife" : {
+        'title': 'Peter Pond, Athabasca\'s early explorer',
+        'storymap_img': '',
+        'description': 'Discover life and exploration of Peter Pond, a controversial figure of the 18th century, who inspired Mackenzie to search the North West Passage.',
+        'lines_name': '',
+        'points_name': 'pondLifePoints',
+        "timeline_html": 'color: cornflowerblue',
+        "img": '',
+        "color": "cornflowerblue"
+    },
+    "castner" : {
+        'title': 'Brian Castner\'s Disappointment River',
+        'storymap_img': '<img src="https://cangeo-media-library.s3.amazonaws.com/s3fs-public/styles/web_article_slider_image/public/images/web_articles/article_images/5183/disappointment_river_graphic.jpg?>',
+        'description': 'In 1789, Alexander Mackenzie traveled 1200 miles on the immense river in Canada that now bears his name, in search of the fabled Northwest Passage that had eluded mariners for hundreds of years. In 2016, the acclaimed memoirist Brian Castner retraced Mackenzie\'s route by canoe in a grueling journey -- and discovered the Passage he could not find. (from Goodreads). Read an extract of the book <a href="https://www.canadiangeographic.ca/article/retracing-legendary-canadian-expedition" target="_blank">here</a>',
+        "lines_name": 'castnerLine',
+        "points_name": 'castnerPoints',
+        "timeline_html": 'color: orchid',
+        "color": "orchid"
+    },
     // "mackenzieLife" : {
     //     'title': 'Alexander Mackenzie: from the Highlands to the North West Territories',
     //     'description': '',
