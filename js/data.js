82e06595efd1f54115c24143c85d8096c823e978
Maria Ricci <mery.ricci.mr@gmail.com>
2021-03-10 11:15:52 +0100

    removed surface temperature, added all stories to data.js, added credits line in sidebar, added about and how it works pages (template)

--- js/data.js
+++ js/data.js
@@ -19,7 +19,7 @@ var storylines = {
     },
     "pondLife" : {
         'title': 'Peter Pond, Athabasca\'s early explorer',
-        'storymap_img': '',
+        'storymap_img': '<img src="https://upload.wikimedia.org/wikipedia/commons/7/74/Fur_traders_in_canada_1777.jpg">',
         'description': 'Discover life and exploration of Peter Pond, a controversial figure of the 18th century, who inspired Mackenzie to search the North West Passage.',
         'points_name': 'pondLifePoints',
         "timeline_html": 'color: cornflowerblue',
@@ -28,19 +28,36 @@ var storylines = {
     },
     "castner" : {
         'title': 'Brian Castner\'s Disappointment River',
-        'storymap_img': '<img src="https://cangeo-media-library.s3.amazonaws.com/s3fs-public/styles/web_article_slider_image/public/images/web_articles/article_images/5183/disappointment_river_graphic.jpg?>',
+        'storymap_img': '<img src="https://cangeo-media-library.s3.amazonaws.com/s3fs-public/styles/web_article_slider_image/public/images/web_articles/article_images/5183/disappointment_river_graphic.jpg">',
         'description': 'In 1789, Alexander Mackenzie traveled 1200 miles on the immense river in Canada that now bears his name, in search of the fabled Northwest Passage that had eluded mariners for hundreds of years. In 2016, the acclaimed memoirist Brian Castner retraced Mackenzie\'s route by canoe in a grueling journey -- and discovered the Passage he could not find. (from Goodreads). Read an extract of the book <a href="https://www.canadiangeographic.ca/article/retracing-legendary-canadian-expedition" target="_blank">here</a>',
         "lines_name": 'castnerLine',
         "points_name": 'castnerPoints',
         "timeline_html": 'color: orchid',
         "color": "orchid"
     },
-    // "mackenzieLife" : {
-    //     'title': 'Alexander Mackenzie: from the Highlands to the North West Territories',
-    //     'description': '',
-    //     "lines_name": '',
+    "mackenzieLife" : {
+        'title': 'Alexander Mackenzie: from the Highlands to the North West Territories',
+        'storymap_img': '<img src="https://upload.wikimedia.org/wikipedia/commons/4/4d/Alexander_MacKenzie_by_Thomas_Lawrence_%28c.1800%29.jpg">',
+        'description': 'Learn how the explorer of the North found his way from Scotland to the remote fur trade posts on Lake Athabasca.',
+        "points_name": 'mackenzieLifePoints',
+        "timeline_html": 'color: chocolate',
+        "color": "chocolate"
+    },
+    // "mackenzie" : {
+    //     'title': 'Alexander Mackenzie: the quest for the Northwest Passage',
+    //     'storymap_img': '<img src="https://www.cwjefferys.ca/uploads/thumbnails/mackenzieatthearctic960.jpg.cad2c5e9.jpg#.YEiHcTBfxPc.link">',
+    //     'description': 'Follow Mackenzie on his journey down the Deh Cho, the Great River that today bears his name.',
     //     "points_name": 'mackenzieLifePoints',
     //     "timeline_html": 'color: chocolate',
     //     "color": "chocolate"
+    // },
+    // "mackay" : {
+    //     'title': '',
+    //     'storymap_img': '<img src="">',
+    //     'description': '',
+    //     "lines_name": '',
+    //     "points_name": '',
+    //     "timeline_html": '',
+    //     "color": ""
     // }
 }
